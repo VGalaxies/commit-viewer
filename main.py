105e36a45cd24f8d282349b1feceff574ee2aef9
V_Galaxy <1904821183@qq.com>
2022-06-10 15:49:27 +0800

    refactor: add func, use csv

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-import os
 import github
+
+import os
 import json
-import pickle
+import csv
 
 from datetime import datetime
 
@@ -9,62 +10,85 @@ from rich.console import Console
 from rich.progress import track
 from rich.table import Table
 
+csv_path = "data.csv"
 config_path = "config.json"
-data_path = "raw.data"
 
-date_format = '%Y-%m-%dT%H:%M:%SZ'
-res = dict()
 
-if os.path.exists(data_path) and os.path.getsize(data_path):
-    with open(data_path, "rb") as f:
-        res = pickle.load(f)
-        print(res)
+def display(res):
+    console = Console()
+    console.rule("statistics")
 
-with open(config_path) as f:
-    config = json.load(f)
-username = config.get('username')
-access_token = config.get('access_token')
+    table = Table()
+    table.add_column("Date")
+    table.add_column("Repo")
+    table.add_column("Msg")
 
-g = github.Github(access_token)
-repo_list = [i for i in g.get_user().get_repos()]
+    for (date, repo, msg) in res:
+        table.add_row(str(date), repo, msg)
 
-# for repo_index in track(range(len(repo_list))):
-for repo_index in track(range(4)):
-    repo = repo_list[repo_index]
+    console.print(table)
 
-    repo_name = str(repo).replace('Repository(full_name="', '').replace('")', '')
-    commit_list = repo.get_commits(author=username)
 
-    try:  # empty repo
-        for page_index in range(commit_list.totalCount):
-            commit_page = commit_list.get_page(page_index)
+def parse_config(path):
+    with open(path) as f:
+        config = json.load(f)
+    username = config.get('username')
+    access_token = config.get('access_token')
+    return username, access_token
 
-            for commit in commit_page:
-                commit_info = commit.raw_data['commit']
 
-                name = commit_info['author']['name']
-                date = commit_info['author']['date']
-                msg = commit_info['message']
+def read_csv(path):
+    if os.path.exists(path) and os.path.getsize(path):
+        res = []
+        with open(csv_path, "r") as f:
+            for row in csv.reader(f, delimiter="|"):
+                res.append(row)
+            return res
+    return None
 
-                res[datetime.strptime(date, date_format)] = [repo_name, msg]
 
-    except github.GithubException:
-        pass
+def write_csv(path, res):
+    with open(path, "w") as f:
+        csv.writer(f, delimiter="|").writerows(res)
 
-res = sorted(res.items(), key=lambda d: d[0])
 
-with open(data_path, "wb") as f:
-    pickle.dump(res, f)
+def fetch():
+    res = read_csv(csv_path)
+    if res is not None:
+        return res
 
-console = Console()
-console.rule("statistics")
+    res = []
+    date_format = '%Y-%m-%dT%H:%M:%SZ'
 
-table = Table()
-table.add_column("Date")
-table.add_column("Repo")
-table.add_column("Msg")
+    username, access_token = parse_config(config_path)
+    g = github.Github(access_token)
+    repo_list = [i for i in g.get_user().get_repos()]
 
-for (date, [repo, msg]) in res:
-    table.add_row(str(date), repo, msg)
+    # for repo_index in track(range(len(repo_list))):
+    for repo_index in track(range(4)):
+        repo = repo_list[repo_index]
 
-console.print(table)
+        repo_name = str(repo).replace('Repository(full_name="', '').replace('")', '')
+        commit_list = repo.get_commits(author=username)
+
+        try:  # empty repo
+            for page_index in range(commit_list.totalCount):
+                commit_page = commit_list.get_page(page_index)
+
+                for commit in commit_page:
+                    commit_info = commit.raw_data['commit']
+
+                    date = commit_info['author']['date']
+                    msg = commit_info['message']
+
+                    res.append((datetime.strptime(date, date_format), repo_name, msg))
+
+        except github.GithubException:
+            pass
+
+    res.sort(key=lambda t: t[0])
+    write_csv(csv_path, res)
+    return res
+
+
+display(fetch())
